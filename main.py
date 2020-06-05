9ddd1d0444b31ea24ae98b5cec8ea5529544bc95
Rishikesh <contactrshikeshhere@gmail.com>
2020-06-06 00:55:39 +0530

    motion detection is working

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 import cv2, time
 
-video=cv2.VideoCapture(0)
+first_frame=None
 
-count=0
+video=cv2.VideoCapture(0)
  
 while True:
     check, frame = video.read()
-
-    print(check)
-    print(frame)
-
+    status=0
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray=cv2.GaussianBlur(gray,(21,21),0)
 
-    #time.sleep(3)
-    cv2.imshow("capturing",gray)
+    if first_frame is None:
+        first_frame = gray
+        continue
+        
+    delta_frame=cv2.absdiff(first_frame,gray)
+    thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame=cv2.dilate(thresh_frame,None,iterations=2)
 
+    (cnts,_) = cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        status=1
+        (x,y,w,h)=cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,0),3)
+
+    cv2.imshow("capturing gray frame",gray)
+    cv2.imshow("capturing blur frame",delta_frame)
+    cv2.imshow("capturing threshold frame",thresh_frame)
+    cv2.imshow("color frame", frame)
+    
     key=cv2.waitKey(1)
 
     if key==ord('q'):
         break
-    count+=1
-print(count)    
+
+    print(status)
+    
 video.release()
 cv2.destroyAllWindows
