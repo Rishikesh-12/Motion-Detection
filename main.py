a4fc64858c83f3cdb5745f581dd02ee0bbbf3be3
Rishikesh <contactrshikeshhere@gmail.com>
2020-06-06 01:52:28 +0530

    added status and timings

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import cv2, time
+import cv2, time, pandas
+from datetime import datetime
 
 first_frame=None
+status_list=[None,None]
+times=[]
+df=pandas.DataFrame(columns=["Start","End"])
 
 video=cv2.VideoCapture(0)
  
@@ -21,11 +25,20 @@ while True:
     (cnts,_) = cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
         status=1
+
         (x,y,w,h)=cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,0),3)
+    status_list.append(status)
+
+
+    if status_list[-1]==1 and status_list[-2]==0:
+        times.append(datetime.now())
+    if status_list[-1]==1 and status_list[-2]==0:
+        times.append(datetime.now())
+
 
     cv2.imshow("capturing gray frame",gray)
     cv2.imshow("capturing blur frame",delta_frame)
@@ -35,9 +48,17 @@ while True:
     key=cv2.waitKey(1)
 
     if key==ord('q'):
+        if status==1:
+            times.append(datetime.now())
         break
 
-    print(status)
-    
+print(status_list)
+print(times)
+
+for i in range(0,len(times),2):
+    df=df.append({"Start":times[i], "End":times[i+1]},ignore_index=True)
+
+df.to_csv("Times.csv")
+
 video.release()
 cv2.destroyAllWindows
